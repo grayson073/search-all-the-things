3579c62be7ab71c6d34ba55f260b468befceb224
Kevin Grayson <k.grayson.ms@gmail.com>
2018-08-15 17:39:47 -0700

    Basic react up and running

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+  render() {
+
+    return (
+      <main>
+        <section>
+          <h2>SUPER REACT</h2>
+        </section>
+      </main>
+    );
+  }
+}
+
+export default App;
