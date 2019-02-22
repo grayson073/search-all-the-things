d4a00b97abddec52d14bdbc0ab90121e606c3001
Kevin Grayson <k.grayson.ms@gmail.com>
2019-02-21 16:45:06 -0800

    Update build

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
   output: {
     path,
     filename: 'bundle.[hash].js',
-    publicPath: '/search-all-the-things/'
+    publicPath: '/'
   },
   // mode (will eventually be cmd line arg in package.json)
   mode: 'development',
